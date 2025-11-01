992f1ad8f533e45b61b315ecb6a55f2488040ee3
Naqibullah <info.naqibullahsarwari@gmail.com>
2025-11-01 09:03:52 +0500

    Backend Setup!

--- server/server..js
+++ server/server..js
@@ -1,7 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+dotenv.config();
 
 const server = express()
 
+server.use(cors());
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
 
 
-server.listen(8080, (req, res) => console.log("The server is running on PORT 8080"))
+
+server.listen(process.env.PORT, (req, res) => console.log(`Server is running on PORT ${process.env.PORT}` ))
